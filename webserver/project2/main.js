7825b7cd5eb1ecdf310e3a77f497954ae67a9706
LeonhartYan <115870323+LeonhartYan@users.noreply.github.com>
2024-09-26 13:32:36 -0400

    1

--- webserver/project2/main.js
+++ webserver/project2/main.js
@@ -1,23 +1,30 @@
 let next;
 let countdown;
 let expect_run = true;
+let i = 0;
+let count = 0;
 
 window.onload = () => {
-    var button = document.getElementById("start");
+    var button1 = document.getElementById("start");
     var panel = document.getElementsByClassName("box1");
     document.getElementById('current-time').textContent = "00:00:00";
     document.getElementById('next-time').textContent = "00:00:00";
     document.getElementById('timer').textContent = "25:00";
     clock_timer = setInterval(now_clock, 1000);
-    button.addEventListener("click", function () {
-        panel[0].style.backgroundColor = 'firebrick';
-        const now = new Date();
-        next = new Date(now.getTime() + 1500000);
-        nextString = next.toLocaleTimeString();
-        expect_run = false;
-        document.getElementById('next-time').textContent = nextString;
-
-        countdown = setInterval(pomodoro, 1000)  
+    button1.addEventListener("click", function () {
+        count += 1;
+        if(count % 2 != 0){
+            panel[0].style.backgroundColor = '#c10038';
+            document.body.style.backgroundColor = '#df3b57';
+            button1.innerHTML = 'Reset Pomodoro Timer<br>00:00'
+            const now = new Date();
+            next = new Date(now.getTime() + 1500000);
+            next_justify = new Date(next.getTime() + 2000)
+            nextString = next_justify.toLocaleTimeString();
+            expect_run = false;
+            document.getElementById('next-time').textContent = nextString;
+            countdown = setInterval(pomodoro, 1000)
+        }
     })
     
 }
@@ -42,11 +49,16 @@ function pomodoro() {
 
 function timer(now_timer) {
     let interval = (next - now_timer) / 1000 + 2;
-    if (interval <= 0) {
+    var panel = document.getElementsByClassName("box1");
+    if (interval <= 0 || count % 2 == 0) {
         expect_run = true;
+        document.getElementById("start").innerHTML = 'Start Pomodoro Timer<br>25:00'
         document.getElementById('timer').textContent = "00:00";  
-        alert("Time's UP!")
+        panel[0].style.backgroundColor = '#fff0f0';
+        document.body.style.backgroundColor = '#ffffff';
+        document.getElementById('timer').textContent = "25:00"; 
         clearInterval(countdown);
+        count = 0;
         return;
     }
     let second = Math.floor(interval % 60);
