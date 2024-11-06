d2be32e75e39c5754675692d5677c35588089d2a
LeonhartYan <115870323+LeonhartYan@users.noreply.github.com>
2024-11-05 20:10:45 -0500

    1

--- Class18-Demo/server.js
+++ Class18-Demo/server.js
@@ -1,48 +1,49 @@
 // how do we know this is a npm project?
-// A: 
+// A: package.json file
 
 // what command do we run to start an npm project?
-// A:
+// A: npm init
 
 // what does the below chunk of code do?
-// A: 
-const express = require("express"); // 
-const multer = require("multer");   // 
-const bodyParser = require("body-parser");  // 
+// A: import libraries
+const express = require("express"); // express
+const multer = require("multer");   // file-upload
+const bodyParser = require("body-parser");  // have body in server request， read body
 
-// 
+// translates bits and bytes(literal memory data to server reable)
 const urlEncodedParser = bodyParser.urlencoded({ extended: true }); 
 
 // what is app?
-// A: 
+// A: apply express framework
 const app = express();
 
 // what is this configuring?
-// A: 
+// A: file upload destination
 const upload = multer({
   dest: "public/uploads",
 });
 
 // what do each of these statements do?
-app.use(express.static("public"));  // 
-app.use(urlEncodedParser);        // 
-app.set("view engine", "ejs"); // 
+app.use(express.static("public"));  // set default folder of static file(assets, img, etc.)
+app.use(urlEncodedParser);        //init parser to translate bit/bytes
+app.set("view engine", "ejs"); // init template engine for site rendering
 
 // what is this?
-// A:
+// A: route handle client request
 app.get("/", (request, response) => {
   response.send("server working");
 
   // what steps do we need in order to use a template ejs file?
-  // 
+  request.render('index.ejs', {})
+  // install ejs, create view folder, set view engine to "ejs", create the .ejs file
   
   // make sure to comment out the res.send() code above
 });
 
 // what does the number signify?
-// A: 
+// A: port number
 // how do we access this on the web?
-// A: 
+// A: [IPAddress]:port
 app.listen(6001, () => {
   console.log("server started on port 6001");
 });
